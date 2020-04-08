e692999a0c0ffd61de88b65dbbb7ddbe50f3f0d2
bayuiqballl <bayuiqballl13@gmail.com>
2020-04-09 02:39:36 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+import Nav from "./component/Nav";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" exact component={Shop} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./../App.css";
+
+function Shop() {
+  return (
+    <div className="Shop">
+      <h1>Shop</h1>
+    </div>
+  );
+}
+
+export default Shop;
